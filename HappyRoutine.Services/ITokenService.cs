4e2261d19ef1fba74e20eddcd8b17d0c130f3ed9
ifk92 <faboivan@yahoo.fr>
2023-01-23 20:40:30 +0100

    Fin implemenentation des services

---
+++ HappyRoutine.Services/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using HappyRoutine.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyRoutine.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+
+    }
+}
