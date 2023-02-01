5f7393bf50c72aad17469ce3427a15cd56262099
ifk92 <faboivan@yahoo.fr>
2023-02-01 18:54:10 +0100

    Generer les composants necessaires.

--- HappyRoutine-UI/src/app/app.module.ts
+++ HappyRoutine-UI/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post-components/post/post.component';
+import { PostCardComponent } from './components/post-components/post-card/post-card.component';
+import { PostEditComponent } from './components/post-components/post-edit/post-edit.component';
+import { CommentBoxComponent } from './components/comment-components/comment-box/comment-box.component';
+import { CommentSystemComponent } from './components/comment-components/comment-system/comment-system.component';
+import { CommentsComponent } from './components/comment-components/comments/comments.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostCardComponent,
+    PostEditComponent,
+    CommentBoxComponent,
+    CommentSystemComponent,
+    CommentsComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
